9567ebf8e0317e1d971891657c5456d9c97d7fdf
seblex9 <seblex@proton.me>
2023-10-19 20:55:08 +0800

    Save JSON info to variables and insert into FOR loop with string template literals for readability

--- script.js
+++ script.js
@@ -5,15 +5,28 @@ document.addEventListener('DOMContentLoaded', function () {
     .then(res => res.json())
     .then(posts => {
       posts.forEach(post => {
+        const date = new Date(post.data);
+        const formattedDate = `${date.getDate()} ${date.toLocaleString(
+          'default',
+          { month: 'long' }
+        )} ${date.getFullYear}}`;
+        const cardImage = post.featured_media;
+        const authorName = post['_embedded']['author'][0]['name'];
+        const authorLink = post['_embedded']['author'][0]['link'];
+        const categoryName =
+          post['_embedded']?.['wp:term']?.[2]?.[0]?.['name'] || 'UNCATEGORIZED';
+        const fullPostLink = post.link;
+        const postTitle = post.title.rendered;
+
         const card = `
 					<div class="col-4 u-equal-height">
 						<div class="p-card u-display-flex">
-							<h5 class="category-name"></h5>
+							<h5 class="category-name">${categoryName}</h5>
 							<hr class="separator">
-							<img class="p-card__image" src="" alt="Featured Image">
+							<img class="p-card__image" src="${cardImage}" alt="Featured Image">
 							<div class="p-card__inner u-flex-grow">
-								<h3><a href=""></a></h3>
-								<p><em>By <a href=""></a> on </em></p>
+								<h3><a href="${fullPostLink}">${postTitle}</a></h3>
+								<p><em>By <a href="${authorLink}">${authorName}</a> on </em></p>
 							</div>
 							<hr class="separator">
 							<div class="p-card__footer">
